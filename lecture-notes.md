8371caf41b3ac6e750d9185ecb387dd341e39536
andre.bonfatti <andrebnf@gmail.com>
2017-09-06 15:25:26 -0300

    Some nice testing

--- lecture-notes.md
+++ lecture-notes.md
@@ -3,3 +3,11 @@
 `mix phx.gen.context Menu Category categories name:string`
 
 `mix phx.gen.schema Menu.Item items name:string description:string category_id:references:categories`
+
+`menuItems(matching: String): [MenuItem]` # returns a list of MenuItem
+
+`non_null(:string)` non_null -> modifier
+
+`get` for queries
+
+`post` for mutations
--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,8 +1,14 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
+  import Ecto.Query
+  alias PlateSlate.{Menu, Repo}
+
   query do
-    field :health, :string
+    field :menu_items, list_of(:menu_item) do
+      arg :matching, :string
+      resolve fn _, args, _ -> menu_item_resolver(nil, args, nil) end
+    end
   end
 
   @desc """
@@ -13,5 +19,17 @@ defmodule PlateSlateWeb.Schema do
     @desc "Name description"
     field :name, :string
     field :description, :string
+    field :price, :float
   end
+
+
+  defp menu_item_resolver(_, %{matching: term}, _) do
+    query =
+      Menu.Item
+      |> where([item], ilike(item.name, ^"%#{term}%"))
+
+    {:ok, Repo.all(query)}
+  end
+  defp menu_item_resolver(_, _, _), do: {:ok, Repo.all(Menu.Item)}
+
 end
